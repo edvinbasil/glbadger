bc3a5f3c7f8516297c0e1b9b3fb49110e4bb19e8
edvinbasil <edvinbasil@gmail.com>
2019-05-13 16:22:35 +0530

    Setup templating engine
    - Using pug
    - Custom theme built off of Milligram
    - Form parsing done

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const badgen = require('badgen')
+const path = require('path')
+
 const db = require('./db')
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
+app.set('views', './src/views')
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'pug');
 
 
 let e404 =''
@@ -17,6 +24,19 @@ const svgString = badgen({
   style: 'flat',
 })
 db.put('svg', svgString)
+
+app.get('/', (req,res) => {
+  res.render('pages/index')
+})
+
+app.post('/gitlab', (req,res) => {
+    const data = req.body.link.split('/')
+  res.json({
+    link: req.body.link,
+    namespace: data[3],
+    project: data[4] 
+  })
+})
 
 app.get('/:id', async (req,res) => {
   res.set('Content-Type', 'image/svg+xml')
