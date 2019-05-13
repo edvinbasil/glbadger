8f577f9215d88b75bbd365a2a08bd74d30dea78f
edvinbasil <edvinbasil@gmail.com>
2019-05-13 19:46:52 +0530

    Parse gitlab pipeline webhook
    - Add /hook to parse pipeline
    - Object stored as json
    - /id to get basic info
    - /id/type serves the type from data

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const badgen = require('badgen')
 const path = require('path')
-
 const db = require('./db')
 
 const app = express()
@@ -17,32 +16,54 @@ let e404 =''
 db.get('error').then(res => {
   e404 = res})
 
-const svgString = badgen({
-  subject: 'hello',
-  status: 'world',
-  color: '0ff',
-  style: 'flat',
-})
-db.put('svg', svgString)
+// badgen({
+//   subject: 'hello',
+//   status: 'world',
+//   color: '0ff',
+//   style: 'flat',
+// })
 
 app.get('/', (req,res) => {
   res.render('pages/index')
 })
 
-app.post('/gitlab', (req,res) => {
-    const data = req.body.link.split('/')
-  res.json({
-    link: req.body.link,
-    namespace: data[3],
-    project: data[4] 
-  })
+app.post('/hook', (req,res) => {
+  const kind = req.body.object_kind;
+  let dat = {}
+  if(kind === 'pipeline'){
+    dat.status = req.body.object_attributes.detailed_status;
+    dat.id = req.body.project.id;
+    dat.time = req.body.object_attributes.finished_at;
+    dat.color = dat.status == 'passed' ? 'green' : 'orange'
+    dat.pipeline = badgen({
+      subject: 'pipeline',
+      status: dat.status,
+      color: dat.color,
+      style: 'flat'
+    })
+    json = JSON.stringify(dat)
+    db.put(dat.id, json)
+  }
+  res.end()
+  console.log(req.body)
 })
 
 app.get('/:id', async (req,res) => {
-  res.set('Content-Type', 'image/svg+xml')
-  const svg = await db.get(req.params.id)
-  if(svg) res.send(svg)
-  else res.send(e404)
+  const dat = await db.get(req.params.id)
+  const json = JSON.parse(dat)
+  res.render('pages/project', json)
+})
+app.get('/:id/:type', async (req,res) => {
+  const dat = await db.get(req.params.id)
+  const json = JSON.parse(dat)
+  // console.log(json)
+  const type = req.params.type
+  console.log(type)
+  if(type in json){
+    res.set('Content-Type', 'image/svg+xml')
+    res.send(json[type])
+  }  
+  else res.status(404).send('Not Found')
 })
 
 app.listen(8000)
